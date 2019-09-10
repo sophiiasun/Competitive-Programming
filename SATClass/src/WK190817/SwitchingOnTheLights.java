50d4e174f77855615a7b20bd8b29c0129c451cc2
sophiiasun <M@rkh@m2014>
2019-09-10 14:03:16 -0400

    new codes

--- SATClass/src/WK190817/SwitchingOnTheLights.java
+++ SATClass/src/WK190817/SwitchingOnTheLights.java
@@ -3,10 +3,16 @@ package WK190817;
 import java.util.*;
 
 public class SwitchingOnTheLights {
+
+    ArrayList<int[]> link = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
         int[][] grid = new int[N][N];
+        for(int i = 0; i < M; i++) {
+            
+        }
     }
 }
