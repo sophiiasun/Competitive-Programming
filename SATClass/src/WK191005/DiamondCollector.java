677138a24d3f2eb5a6d18c1c7f74c89aa8b9bd49
sophiiasun <M@rkh@m2014>
2019-10-05 16:18:32 -0400

    Update DiamondCollector.java

--- SATClass/src/WK191005/DiamondCollector.java
+++ SATClass/src/WK191005/DiamondCollector.java
@@ -7,45 +7,15 @@ public class DiamondCollector {
     private static int N;
     private static int K;
     private static int[] arr;
+
     public static void main(String[] args) {
-        arr = new int[N];
-        for (int i = 0; i < N; i++) arr[i] = sc.nextInt();
-        Arrays.sort(arr);
         N = sc.nextInt();
         K = sc.nextInt();
-        System.out.println ("How many cases?");
-        int in = sc.nextInt();
-        if (in == 1) oneCase();
-        else if (in == 2) twoCase();
-        else System.out.println("Error. Please re-run.");
-    }
-
-    private static void twoCase() {
-        int[] data = new int[N];
-        int mrk = 0;
-        for (int i = 0; i < N; i++) {
-            mrk = i + 1;
-            int cnt = 1;
-            while (mrk < N) {
-                if (arr[mrk] - arr[i] <= K)
-                    cnt++;
-                else break; //mrk reaches the closet but unavailable int
-                mrk++;
-            }
-            data[i] = cnt;
-        }
-        int max = 0, mrk2 = 0; //mrk2 stores the starting position of the largest sub-arr
-        for (int i = 0; i < N; i++)
-            if (data[i] > max) {
-                max = data[i];
-                mrk2 = i;
-            }
-        for (int i = 0; i < mrk; i++) {
-            
-        }
-        for (int i = 0; i < mrk2; i++) {
-
-        }
+        arr = new int[N];
+        for(int i = 0; i < N; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        twoCase();
     }
 
     private static void oneCase() {
@@ -66,5 +36,35 @@ public class DiamondCollector {
             if (data[i] > max) max = data[i];
         }
         System.out.println(max); // 1 1 3 4 6
+    }
+
+    private static void twoCase() {
+        int max = 0;
+        int[] scan1 = new int[N];
+        int[] scan2 = new int[N];
+        for (int i = 1; i < N - 1; i++) { // i is the beginning index of the second half
+            int cnt = 0;
+            for (int j = i - 1; j >= 0; j--) { // check for i-1 as the largest value
+                if (arr[i - 1] - arr[j] <= K) cnt++;
+                else {
+                    scan1[i - 1] = cnt;
+                    break;
+                }
+            }
+            cnt = 0;
+            for (int j = i; j < N; j++) { //check for i as the smallest value
+                if (arr[j] - arr[i] <= K) cnt++;
+                else {
+                    scan2[i] = cnt;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for(int j = i + 1; j < N; j++) {
+                if (i + 1 > max) max = i + j;
+            }
+        }
+        System.out.println(max);
     }
 }
