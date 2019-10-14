afc643dd1f7b7777d9aa6656a961ea5108543c7a
sophiiasun <M@rkh@m2014>
2019-10-14 10:05:00 -0400

    Update Sudoku.java

--- SATClass/src/WK191012/Sudoku.java
+++ SATClass/src/WK191012/Sudoku.java
@@ -3,7 +3,16 @@ package WK191012;
 import java.util.*;
 
 public class Sudoku {
+    int[][] arr = new int[9][9];
     public static void main(String[] args) {
 
+    }
+    static void init() {
+        Scanner sc = new Scanner(System.in);
+        for(int r = 0; r < 9; r++) {
+            for(int c = 0; c < 9; c++) {
+                
+            }
+        }
     }
 }
