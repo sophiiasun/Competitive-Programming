d9c45c48739ae52ffde77d07f45218864a0e791b
sophiiasun <M@rkh@m2014>
2019-11-29 08:24:37 -0500

    computer purchase completed

--- SATClass/src/WK191123/ComputerPurchase.java
+++ SATClass/src/WK191123/ComputerPurchase.java
@@ -3,46 +3,39 @@ package WK191123;
 import java.util.*;
 
 public class ComputerPurchase {
-    static int N;
-    static String[] name;
-    static int[][] arr;
     static String str1, str2;
+    static int max1, max2;
     public static void main(String[] args) {
         run();
-        if (str1 != null)
-            System.out.println(str1);
-        if (str2 != null)
+        if (str1 != null && max1 == max2 && str1.compareTo(str2) > 0) {
             System.out.println(str2);
+            System.out.println(str1);
+        } else {
+            if (str1 != null)
+                System.out.println(str1);
+            if (str2 != null)
+                System.out.println(str2);
+        }
     }
     static void run(){
-        int max = 0;
-        String last;
         int N, R, S, D; //ram, speed, drive
         String B; // brand
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        if (N == 0)
-            System.exit(1);
         for (int i = 0; i < N; i++) {
             B = sc.next();
-            if (N == 1) {
-                System.out.println(B);
-                System.exit(1);
-            }
             R = sc.nextInt();
             S = sc.nextInt();
             D = sc.nextInt();
             int tmp = (R<<1) + (S<<1) + S + D;
-            if (tmp > max) {
-                last = str1;
+            if (tmp > max1) {
+                max2 = max1;
+                max1 = tmp;
+                str2 = str1;
                 str1 = B;
-                str2 = last;
-                max = tmp;
-            } else if (tmp == max) {
-                if(B.charAt(0) < str1.charAt(0)) {
-                    str2 = str1;
-                    str1 = name[i];
-                }
+            } else if (tmp > max2){
+                max2 = tmp;
+                str2 = B;
             }
         }
     }
