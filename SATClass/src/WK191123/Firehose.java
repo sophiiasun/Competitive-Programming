a4d5ac92d6dc312357f1a95fa0dbdc88632ca3e1
sophiiasun <M@rkh@m2014>
2019-12-01 22:27:05 -0500

    Update Firehose.java

--- SATClass/src/WK191123/Firehose.java
+++ SATClass/src/WK191123/Firehose.java
@@ -3,9 +3,23 @@ package WK191123;
 import java.util.*;
 
 public class Firehose {
-    static int H;
-
+    static int H, K;
+    static int[] arr;
+    static int[] dA; //contains the difference from i to i+1
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        H = sc.nextInt();
+        arr = new int[K];
+        dA = new int[K];
+        int max = 0, tmp;
+        for (int i = 0; i < K; i++) {
+            arr[i] = sc.nextInt();
+            if (i != 0) {
+                dA[i-1] = arr[i] - arr[i-1];
+            }
+        }
+        dA[K-1] = 1000000 - arr[K-1];
+        K = sc.nextInt();
+        
     }
 }
