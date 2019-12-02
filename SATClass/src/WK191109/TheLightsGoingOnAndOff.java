9d9a3cf11ab59160bc795b6c072da6ddcf5dda38
sophiiasun <M@rkh@m2014>
2019-12-01 20:06:53 -0500

    done huffman

--- SATClass/src/WK191109/TheLightsGoingOnAndOff.java
+++ SATClass/src/WK191109/TheLightsGoingOnAndOff.java
@@ -31,5 +31,6 @@ public class TheLightsGoingOnAndOff {
             }
             arr[r] = tmp;
         }
+
     }
 }
---
+++ SATClass/src/WK191123/Firehose.java
@@ -0,0 +1,11 @@
+package WK191123;
+
+import java.util.*;
+
+public class Firehose {
+    static int H;
+
+    public static void main(String[] args) {
+
+    }
+}
