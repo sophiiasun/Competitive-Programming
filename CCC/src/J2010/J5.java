3a11643b2ab909de07411bce5f417491ed19d9d9
sophiiasun <M@rkh@m2014>
2019-12-08 14:30:11 -0500

    Update J5.java

--- CCC/src/J2010/J5.java
+++ CCC/src/J2010/J5.java
@@ -3,51 +3,56 @@ package J2010;
 import java.util.*;
 
 public class J5 {
-    static int Xs, Ys, Xe, Ye;
+    static int XS, YS, XE, YE;
     static int[] addX = {-2, -2, -1, 1, 2, 2, 1, -1};
     static int[] addY = {-1, 1, 2, 2, 1, -1, -2, -2};
-    static Queue<ArrayList<int[]>> aQ;
+    static Queue<ArrayList<int[]>> q;
     static int[][] map = new int[8][8];
+
     public static void main(String[] args) {
         init();
-        if (Xs == Xe && Ys == Ye)
+        if (XS == XE && YS == YE)
             System.out.println(0);
         else
             run();
     }
+
     static void run(){
         int counter = 1;
-        aQ = new LinkedList<>();
-        ArrayList<int[]> add = new ArrayList<>();
-        add.add(new int[]{Xs, Ys});
-        aQ.add(add);
-        while (!aQ.isEmpty()) {
-            add = new ArrayList<>();
-            ArrayList<int[]> next = aQ.poll();
+        q = new LinkedList<>();
+        ArrayList<int[]> list = new ArrayList<>();
+        list.add(new int[]{XS, YS});
+        q.add(list);
+        while (!q.isEmpty()) {
+            list = new ArrayList<>();
+            ArrayList<int[]> next = q.poll();
             for (int[] arr : next) {
                 for (int i = 0; i < 8; i++)
-                    updateVal(arr[0] + addX[i], arr[1] + addY[i], counter, add);
+                    updateVal(arr[0] + addX[i], arr[1] + addY[i], counter, list);
             }
             counter++;
-            aQ.add(add);
+            q.add(list);
         }
     }
+
     static void updateVal(int iX, int iY, int iC, ArrayList<int[]> add){
         if (iX >= 0 && iX < 8 && iY>=0 && iY<8) {
-            if (iX == Xe && iY == Ye) {
+            if (iX == XE && iY == YE) {
                 System.out.println(iC);
                 System.exit(0);
-            }
-            else if (map[iX][iY]  == 0) {
+            } else if (map[iX][iY]  == 0) {
                 add.add(new int[]{iX, iY});
                 map[iX][iY] = iC;
             }
         }
     }
+
     static void init(){
         Scanner sc = new Scanner(System.in);
-        Xs = sc.nextInt() - 1; Ys = sc.nextInt() - 1;
-        Xe = sc.nextInt() - 1; Ye = sc.nextInt() - 1;
-        map[Xs][Ys] = -1;
+        XS = sc.nextInt() - 1;
+        YS = sc.nextInt() - 1;
+        XE = sc.nextInt() - 1;
+        YE = sc.nextInt() - 1;
+        map[XS][YS] = -1;
     }
 }
