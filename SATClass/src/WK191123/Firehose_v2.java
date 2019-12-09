4628588d813f2c83a501e54b6836a2fc22776991
sophiiasun <M@rkh@m2014>
2019-12-08 23:02:12 -0500

    Create Firehose_v2.java

---
+++ SATClass/src/WK191123/Firehose_v2.java
@@ -0,0 +1,56 @@
+package WK191123;
+
+import java.util.*;
+
+public class Firehose_v2 {
+    static int H, K;
+    static int[] arr;
+    public static void main(String[] args) {
+        init();
+        run();
+    }
+    static void run(){
+        int min = 0, max = 500000;
+        while (min < max) {
+            int mid = (min+max)>>1;
+            int num = findNumHydrant(mid);
+            if (num >= K)
+                max = mid;
+            else
+                min = mid + 1;
+        }
+        System.out.println(min);
+    }
+    static int findNumHydrant(int len){
+        int min = Integer.MAX_VALUE;
+        for (int start = 0; start < H; start++){ //start = starting index
+            int tmp = 1, curr = start; //curr = current location
+            for (int i = curr; i < H + start; i++) {
+                if (arr[i] - arr[curr] > len * 2){
+                    curr = i;
+                    tmp++;
+                }
+            }
+            min = Math.max(min, tmp);
+        }
+        return min;
+    }
+    static void init(){
+        Scanner sc = new Scanner(System.in);
+        H = sc.nextInt();
+        arr = new int[H * 2];
+        for (int i = 0; i < H; i++)
+            arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        for (int i = 0; i < H; i++)
+            arr[i + H] = arr[i] + 1000000;
+        K = sc.nextInt();
+    }
+}
+
+/*
+if (num == K) {
+                System.out.println(mid);
+                return;
+            } else
+ */
