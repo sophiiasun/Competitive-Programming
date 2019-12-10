89286f1ca5ac18a8a7db955cd0fb4eba79afa043
sophiiasun <M@rkh@m2014>
2019-12-09 22:20:06 -0500

    firehose done

--- SATClass/src/WK191123/Firehose_v2.java
+++ SATClass/src/WK191123/Firehose_v2.java
@@ -5,25 +5,25 @@ import java.util.*;
 public class Firehose_v2 {
     static int H, K;
     static int[] arr;
+
     public static void main(String[] args) {
         init();
         run();
     }
+
     static void run(){
         int min = 0, max = 500000;
         while (min < max) {
             int mid = (min+max)>>1;
             int num = findNumHydrant(mid);
-            if (num == K) {
-                System.out.println(mid);
-                return;
-            } else if (num >= K)
+            if (K >= num)
                 max = mid;
             else
                 min = mid + 1;
         }
         System.out.println(min);
     }
+
     static int findNumHydrant(int len){
         int min = Integer.MAX_VALUE;
         for (int start = 0; start < H; start++){ //start = starting index
@@ -34,17 +34,19 @@ public class Firehose_v2 {
                     tmp++;
                 }
             }
-            min = Math.max(min, tmp);
+            if (min > tmp)
+                min = tmp;
         }
         return min;
     }
+
     static void init(){
         Scanner sc = new Scanner(System.in);
         H = sc.nextInt();
         arr = new int[H * 2];
         for (int i = 0; i < H; i++)
             arr[i] = sc.nextInt();
-        Arrays.sort(arr);
+        Arrays.sort(arr, 0, H);
         for (int i = 0; i < H; i++)
             arr[i + H] = arr[i] + 1000000;
         K = sc.nextInt();
---
+++ SATClass/src/WK191207/AliceThroughTheLookingLens.java
@@ -0,0 +1,13 @@
+package WK191207;
+
+import java.util.*;
+
+public class AliceThroughTheLookingLens {
+    static int T;
+    public static void main(String[] args) {
+
+    }
+    static void init(){
+
+    }
+}
