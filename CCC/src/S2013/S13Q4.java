7e6c775c1994d8c23b18500d75fec8f142b0527d
sophiiasun <M@rkh@m2014>
2020-01-02 22:49:12 -0500

    Update S13Q4.java
    pass batch 4 and first 2 of batch 5

--- CCC/src/S2013/S13Q4.java
+++ CCC/src/S2013/S13Q4.java
@@ -3,25 +3,59 @@ package S2013;
 import java.util.*;
 
 public class S13Q4 {
+    static Queue<Set<Integer>> queue = new LinkedList<>();
+    static int p, q;
+    static HashMap<Integer, Set<Integer>> map = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(), M = sc.nextInt();
-        HashMap<Integer, Set<Integer>> map = new HashMap<>();
         for (int i = 0; i < M; i++) {
             int key = sc.nextInt();
             if (!map.containsKey(key))
                 map.put(key, new HashSet<>());
             map.get(key).add(sc.nextInt());
         }
-        int p = sc.nextInt(), q = sc.nextInt();
-        if (map.size() != N)
-            System.out.println("unknown");
-        else {
+        p = sc.nextInt();
+        q = sc.nextInt();
 
+        if (map.containsKey(p)) {
+            Set<Integer> set = map.get(p);
+            if (set.contains(q)) {
+                System.out.println("yes");
+                System.exit(0);
+            }
+            queue.add(map.get(p));
+            doBFS(q);
         }
+
+        if (map.containsKey(q)) {
+            if (map.containsKey(q)) {
+                if (map.get(q).contains(p)) {
+                    System.out.println("no");
+                    System.exit(0);
+                }
+            }
+            queue.add(map.get(q));
+            doBFS(p);
+        }
+
+        System.out.println("unknown");
     }
 
-    static void getchild(int num){
-        
+    static void doBFS(int target) {
+        while (!queue.isEmpty()) {
+            Set<Integer> next = queue.poll();
+            for (int i : next) {
+                if (i == target && target == q) {
+                    System.out.println("yes");
+                    System.exit(0);
+                } else if (i == target && target == p) {
+                    System.out.println("no");
+                    System.exit(0);
+                } else if (map.containsKey(i))
+                    queue.add(map.get(i));
+            }
+        }
     }
 }
