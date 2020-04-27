6cc76eed2fe473d294c10156f5e151e095d9f2c2
sophiiasun <M@rkh@m2014>
2020-04-26 21:12:02 -0400

    knapsack

--- CCC/src/Miscellaneous/Atcoder/Frog3.java
+++ CCC/src/Miscellaneous/Atcoder/Frog3.java
@@ -1,4 +1,4 @@
-package Miscellaneous;
+package Miscellaneous.Atcoder;
 
 import java.io.*;
 
--- CCC/src/Miscellaneous/Atcoder/Grid1.java
+++ CCC/src/Miscellaneous/Atcoder/Grid1.java
@@ -1,4 +1,4 @@
-package Miscellaneous;
+package Miscellaneous.Atcoder;
 
 import java.nio.Buffer;
 import java.util.*;
