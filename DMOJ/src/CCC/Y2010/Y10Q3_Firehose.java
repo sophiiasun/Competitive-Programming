2fa66bda01dfe71097db68bbae17179e2363d64b
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2020-08-03 22:38:37 -0400

    Move directories under CCC/2010 out to directories under DMOJ

---
+++ DMOJ/src/CCC/Y2010/Y10Q3_Firehose.java
@@ -0,0 +1,54 @@
+package CCC.Y2010;
+
+import java.util.*;
+
+public class Y10Q3_Firehose {
+    static int H, K;
+    static int[] arr;
+
+    public static void main(String[] args) {
+        init();
+        run();
+    }
+
+    static void run(){
+        int min = 0, max = 500000;
+        while (min < max) {
+            int mid = (min+max)>>1;
+            int num = findNumHydrant(mid);
+            if (K >= num)
+                max = mid;
+            else
+                min = mid + 1;
+        }
+        System.out.println(min);
+    }
+
+    static int findNumHydrant(int len){
+        int min = Integer.MAX_VALUE;
+        for (int start = 0; start < H; start++){ //start = starting index
+            int tmp = 1, curr = start; //curr = current location
+            for (int i = curr; i < H + start; i++) {
+                if (arr[i] - arr[curr] > len * 2){
+                    curr = i;
+                    tmp++;
+                }
+            }
+            if (min > tmp)
+                min = tmp;
+        }
+        return min;
+    }
+
+    static void init(){
+        Scanner sc = new Scanner(System.in);
+        H = sc.nextInt();
+        arr = new int[H * 2];
+        for (int i = 0; i < H; i++)
+            arr[i] = sc.nextInt();
+        Arrays.sort(arr, 0, H);
+        for (int i = 0; i < H; i++)
+            arr[i + H] = arr[i] + 1000000;
+        K = sc.nextInt();
+    }
+}
