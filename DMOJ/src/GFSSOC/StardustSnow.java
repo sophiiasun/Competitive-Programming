db798cc319602cb268aecc6b46c1c8c493e3af9a
sophiiasun <M@rkh@m2014>
2020-08-07 23:18:20 -0400

    update

--- DMOJ/src/GFSSOC/StardustSnow.java
+++ DMOJ/src/GFSSOC/StardustSnow.java
@@ -7,34 +7,24 @@ public class StardustSnow {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<20);
     static StringTokenizer st;
 
-    static int R, C, S, B, K, M, dp[][][][];
-    static ArrayList<C>[] adj;
+    static int R, C, S, B, K, M, dp[][][][], av[][], at[][];
 
     public static void main(String[] args) throws IOException {
         R = readInt(); C = readInt(); S = readInt(); B = readInt(); K = readInt(); M = readInt();
-        dp = new int[C+1][R+1][K+1][B+1]; adj = new ArrayList[R+1];
-        for (int i = 1; i <= R; i++) adj[i] = new ArrayList<>();
-        for (int i = 0, t, v, c; i < S; i++) { t = readInt(); v = readInt(); c = readInt(); adj[readInt()].add(new C(c, t, v));  }
-        for (int i = 1; i <= C; i++) for (int j = 1; j <= R; j++) for (int k = 0; k <= K; k++) Arrays.fill(dp[i][j][k], -1);
+        dp = new int[C+1][R+1][K+1][B+1]; av = new int[C+1][R+1]; at = new int[C+1][R+1];
+        for (int i = 0, t, v, c, r; i < S; i++) { t = readInt(); v = readInt(); c = readInt(); r = readInt(); av[c][r] = v; at[c][r] = t; }
         System.out.println(run(1, 1, 0, 0));
     }
 
     static int run(int p, int h, int k, int t) {
-        if (h == R+1 || k > K || t >= B) return 0;
-        if (dp[p][h][k][t] != -1) return dp[p][h][k][t];
+        if (h == R+1 || k >= K || t >= B) return 0;
+        if (dp[p][h][k][t] > 0) return dp[p][h][k][t];
         int ret = 0;
-        for (C c : adj[h]) {
-            if (Math.abs(p-c.p) <= M ) {
-                ret = Math.max(ret, run(c.p, h+1, k, t)); // don't take
-                if (k < K && t + c.t < B) ret = Math.max(ret, c.v + run(c.p, h+1, k+1, t+c.t));
-            }
+        for (int i = Math.max(p-M, 0); i <= Math.min(p+M, C); i++) { // keep within bounds : i=p
+            ret = Math.max(ret, run(i, h+1, k, t)); // don't take
+            if (k < K && t + at[i][h] < B) ret = Math.max(ret, av[i][h] + run(i, h+1, k+1, t+at[i][h]));
         }
         return dp[p][h][k][t] = ret;
-    }
-
-    static class C {
-        int p, t, v;
-        C (int pp, int tt, int vv) { p = pp; t = tt; v = vv; }
     }
 
     static String next() throws IOException {
