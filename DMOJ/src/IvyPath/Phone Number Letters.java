ab2f0370df7b140ee80211d26302f165dc4a9d17
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2021-04-11 14:13:21 -0400

    upd variety

--- DMOJ/src/IvyPath/Phone Number Letters.java
+++ DMOJ/src/IvyPath/Phone Number Letters.java
@@ -1,19 +1,3 @@
-package IvyPath;
-
-import java.util.*;
-import java.io.*;
-
-/* 1: N/A
- * 2: a, b, c
- * 3: d, e, f
- * 4: g, h, i
- * 5: j, k, l
- * 6: m, n, o
- * 7: p, q, r, s
- * 8: t, u, v
- * 9: w, x, y, z
- */
-
 public class PhoneNumberLetters {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<20);
     static StringTokenizer st;
