fd94b0fcde47560d1d7d17a79806f3646e3603b9
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2021-04-13 01:59:21 -0400

    upload aurora

--- DMOJ/src/DMOPC/Y2014/C8/P5 - Aurora.java
+++ DMOJ/src/DMOPC/Y2014/C8/P5 - Aurora.java
@@ -1,52 +1,40 @@
-import java.io.*;
-import java.util.*;
+#include <bits/stdc++.h>
+#include <iostream>
+#define sc(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define ins insert
 
-public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<20);
-    static StringTokenizer st;
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+typedef map<int, int> mii;
+char _;
 
-    public static void main(String[] args) throws IOException {
-        double TIME = System.currentTimeMillis();
+const int MN = 1e5+3;
+int N, M, arr[MN]; ll A, B, C, t[MN], tt = 0, tb, tc, save=0;
+bool over;
 
-        final int N = readInt(), M = readInt(); long A = readInt(), B = readInt(), C = readInt();
-        int arr[] = new int[N+1]; long time[] = new long[N+1];
-        for (int i = 1; i <= N; i++) arr[i] = readInt();
-        Arrays.sort(arr); // sort array based on where soldiers are headed
-        time[1] = A * (arr[1] - 1); // time[] stores the min time is takes for soldier i to travel to their destination
-        for (int i = 2; i <= N; i++) { // find time required if all soldiers used Aurora
-            if (arr[i] != arr[i-1]) time[i] = A * (arr[i] - arr[i-1]);
-            time[i] += time[i-1] + C;
+int main() {
+    sc(N); sc(M); sc(A); sc(B); sc(C);
+    for (int i = 1; i <= N; i++) sc(arr[i]);
+    sort(arr+1, arr+N+1);
+    t[1] = A * (arr[1] - 1);
+    for (int i = 2; i <= N; i++) {
+        if (arr[i] != arr[i-1]) t[i] = A * (arr[i] - arr[i-1]);
+        t[i] += t[i-1] + C;
+    }
+    for (int i = 1; i <= N; i++) {
+        if (!over) {
+            t[i] -= tt; tb = B * (arr[i] - 1); tc = C * (N - i);
+            if (tb <= tc + t[i]) tt += C, t[i] = tb;
+            else over = 1, save = tt * (N-i);
         }
-        long t = 0, tb, tc, save = 0;
-        boolean over = false;
-        for (int i = 1; i <= N; i++) { // don't need to check N because arr[N] travels to M (Aurora def faster)
-            if (!over) { // if soldier i should take Aurora, then soldier i -> soldier should all take Aurora
-                time[i] -= t; // subtract time bc some soldiers used para-mail and don't gotta wait for them to get off Aurora
-                tb = B * (arr[i] - 1); // time taken if soldier i took para-mail
-                tc = C * (N - i); // time taken for remaining soldiers to get off of Aurora
-                // if tb + time for remaining soldiers via Aurora <= tc + time[i] + time for remaining soldiers via Aurora
-                // OR (SIMPLIFIED): if tb <= tc + time[i]
-                if (tb <= tc + time[i]) {
-                    t += C; time[i] = tb;
-                } else {
-                    over = true; // Everyone remaining should take Aurora
-                    save = t * (N-i);
-                }
-            }
-            time[i] += time[i-1];
-        }
-        System.out.println(time[N] - save);
-
-        TIME = (System.currentTimeMillis() - TIME);
-        if (TIME >= 600) System.out.println(TIME);
+        t[i] += t[i-1];
     }
-
-    static String next() throws IOException {
-        while (st == null || !st.hasMoreTokens())
-            st = new StringTokenizer(br.readLine().trim());
-        return st.nextToken();
-    }
-    static int readInt () throws IOException {
-        return Integer.parseInt(next());
-    }
+    cout<<t[N]-save<<"\n";
 }
